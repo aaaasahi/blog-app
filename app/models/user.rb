8d1c539562cfe13442546cb386e4c11c4bdf13c7
asahi <asap1017@docomo.ne.jp>
2020-12-25 18:18:36 +0900

    user-articleのリレーションを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,4 +21,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  has_many :articles, dependent: :destroy
+
+  def has_witten?(article)
+    articles.exists?(id: article.id)
+  end
+
+  def disp_name
+    self.email.split('@').first
+  end
 end
