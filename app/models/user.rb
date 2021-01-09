9bc45756789ec7045f4217941b73723745cdece8
asahi <asap1017@docomo.ne.jp>
2021-01-09 19:35:22 +0900

    アンフォロー機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -46,17 +46,15 @@ class User < ApplicationRecord
   end
 
   def follow!(user)
-    if user.is_a?(User)
-      user_id = user.id
-    else
-      user_id = user
-    end
+    user_id = get_user_id(user)
 
     following_relationships.create!(following_id: user_id)
   end
 
   def unfollow!(user)
-    relation = following_relationship.find_by!(following_id: user.id)
+    user_id = get_user_id(user)
+
+    relation = following_relationships.find_by!(following_id: user_id)
     relation.destroy!
   end
 
@@ -81,5 +79,14 @@ class User < ApplicationRecord
 
   def prepare_profile
     profile || build_profile
+  end
+
+  private 
+  def get_user_id(user)
+    if user.is_a?(User)
+      user.id
+    else
+      user
+    end
   end
 end
