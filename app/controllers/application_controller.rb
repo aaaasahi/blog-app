cf98d3541e5d097b20cb4ecedd3c4c36495e22dc
asahi <asap1017@docomo.ne.jp>
2021-01-10 10:05:31 +0900

    追加ブランチ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,2 @@
 class ApplicationController < ActionController::Base
-    def current_user
-        ActiveDecorator::Decorator.instance.decorate(super) if super.present?
-        super
-    end
 end
