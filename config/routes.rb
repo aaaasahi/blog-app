5a0f0460604a181361aee366b5bb451bcc829978
asahi <asap1017@docomo.ne.jp>
2021-01-12 10:22:06 +0900

    namespace, scopeを使ってroutesを変更

--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,7 @@ Rails.application.routes.draw do
   root to: 'articles#index'
   resource :timeline, only: [:show]
   
-  resources :articles do
-    resources :comments, only: [:index,:new, :create]
-    
-    resource :like, only: [:show, :create, :destroy]
-  end
+  resources :articles 
 
   resources :accounts, only: [:show] do
     resources :follows, only: [:create]
@@ -20,4 +16,12 @@ Rails.application.routes.draw do
 
   resource :profile, only:[:show,:edit,:update]
   resources :favorites, only:[:index]
+  
+
+  namespace :api, defaults: {fomat: :json} do
+    scope '/articles/:article_id' do
+      resources :comments, only: [:index, :create]
+      resource :like, only: [:show, :create, :destroy]
+    end
+  end
 end
